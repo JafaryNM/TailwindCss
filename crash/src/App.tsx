ada7cfd9a091e7944a02ddf5245ede6db2b2d979
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-11-09 14:02:19 +0300

    theme-tailwind

---
+++ crash/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-white bg-deepblue text-3xl font-bold p-12">
+          React tailwind
+        </h1>
+        <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4">
+          Click here
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
