0d28fbe384b1322f11c25aac39c73e02db1e4e56
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-11-10 09:10:14 +0300

    update -utilities

--- crash/src/App.tsx
+++ crash/src/App.tsx
@@ -5,9 +5,14 @@ function App() {
     <>
       <div className="container bg-deepblue">
         <h1 className="text-white text-3xl font-bold ">React tailwind</h1>
-        <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4">
+        <button className="bg-amber text-green-500  text-2xl font-bold mt-12 rounded border-4 px-6 py-4">
           Click here
         </button>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          className="bg-green-500 text-lg text-white "
+        />
       </div>
     </>
   );
