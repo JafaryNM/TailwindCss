6a4d3b2afed767c6077614366440b231f2023a54
Jafary Mdegela <jaffarymdegela@gmail.com>
2025-11-10 08:38:04 +0300

    utilities in action

--- crash/src/App.tsx
+++ crash/src/App.tsx
@@ -3,10 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-white bg-deepblue text-3xl font-bold p-12">
-          React tailwind
-        </h1>
+      <div className="container bg-deepblue">
+        <h1 className="text-white text-3xl font-bold ">React tailwind</h1>
         <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4">
           Click here
         </button>
